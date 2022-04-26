c460acbd2277b087ca239dba445b6838c22d3b85
Gergő Pistai <gergopistai@gmail.com>
2022-04-26 12:08:53 +0200

    Added tests

--- src/test/java/coins/state/CoinsTest.java
+++ src/test/java/coins/state/CoinsTest.java
@@ -1,5 +1,6 @@
 package coins.state;
 
+import org.apache.commons.math3.util.CombinatoricsUtils;
 import org.junit.jupiter.api.Test;
 
 import java.util.BitSet;
@@ -15,6 +16,23 @@ class CoinsTest {
         BitSet bs = new BitSet(7);
         bs.set(0, 7);
         state2 = new Coins(7, 3, bs);
+    }
+
+    @Test
+    public void getFlipsTest() {
+        assertEquals(state1.getFlips(), Coins.generateFlips(state1.getN(), state1.getM()));
+    }
+
+    @Test
+    public void generateFlipsTest() {
+        for (int i = 1; i < 15; i++)
+            for (int j = 1; j < i - 1; j++)
+                assertEquals(CombinatoricsUtils.binomialCoefficient(i, j), Coins.generateFlips(i, j).size());
+    }
+
+    @Test
+    public void isGoalTest() {
+        assertTrue(state2.isGoal());
     }
 
 }
